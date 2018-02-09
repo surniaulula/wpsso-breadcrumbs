97c4942a8fe92f97058e6735869f28a6c7edde23
JS Morisset <jsm@surniaulula.com>
2018-02-09 09:41:31 -0600

    v1.0.0

--- wpsso-breadcrumbs.php
+++ wpsso-breadcrumbs.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Plugin Name: WPSSO Breadcrumbs
+ * Plugin Name: WPSSO Breadcrumbs Markup
  * Plugin Slug: wpsso-breadcrumbs
  * Text Domain: wpsso-breadcrumbs
  * Domain Path: /languages
