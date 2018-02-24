0a950506d757b10d830d7cdcd9c0c7cca21d29d9
JS Morisset <jsm@surniaulula.com>
2018-02-24 12:29:42 -0600

    v1.1.0

--- lib/config.php
+++ lib/config.php
@@ -16,7 +16,7 @@ if ( ! class_exists( 'WpssoBcConfig' ) ) {
 		public static $cf = array(
 			'plugin' => array(
 				'wpssobc' => array(			// Plugin acronym.
-					'version' => '1.1.0-rc.3',		// Plugin version.
+					'version' => '1.1.0',		// Plugin version.
 					'opt_version' => '2',		// Increment when changing default option values.
 					'short' => 'WPSSO BC',		// Short plugin name.
 					'name' => 'WPSSO Breadcrumbs Markup',
@@ -29,7 +29,7 @@ if ( ! class_exists( 'WpssoBcConfig' ) ) {
 					'req' => array(
 						'short' => 'WPSSO Core',
 						'name' => 'WPSSO Core',
-						'min_version' => '3.54.0-rc.3',
+						'min_version' => '3.54.0',
 					),
 					'img' => array(
 						'icons' => array(
