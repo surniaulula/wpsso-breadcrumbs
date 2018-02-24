a3adc9fffefc350dfa06a50838290b50f138a960
JS Morisset <jsm@surniaulula.com>
2018-02-24 11:45:38 -0600

    v1.1.0-rc.3

--- wpsso-breadcrumbs.php
+++ wpsso-breadcrumbs.php
@@ -207,7 +207,9 @@ if ( ! class_exists( 'WpssoBc' ) ) {
 				'wpsso-breadcrumbs' ), $info['name'], $info['version'], $info['req']['short'], $info['req']['min_version'], $have_version );
 
 			if ( is_admin() ) {
+
 				$this->p->notice->err( $error_msg );
+
 				if ( method_exists( $this->p->admin, 'get_check_for_updates_link' ) ) {
 					$this->p->notice->inf( $this->p->admin->get_check_for_updates_link() );
 				}
