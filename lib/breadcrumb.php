6aa6a20ec0fd33d5f7f946e6852e0cdc0f7bcc4c
JS Morisset <jsm@surniaulula.com>
2018-10-22 07:30:53 -0600

    v1.6.0

--- lib/breadcrumb.php
+++ lib/breadcrumb.php
@@ -108,16 +108,8 @@ if ( ! class_exists( 'WpssoBcBreadcrumb' ) ) {
 
 				$item_count++;
 
-				$item_name = $wpsso->page->get_title(
-					$max_len      = 0,
-					$dots         = '',
-					$mod,
-					$read_cache   = true,
-					$add_hashtags = false,
-					$do_encode    = true,
-					$md_idx       = 'schema_title',
-					$sep          = false
-				);
+				$item_name = $wpsso->page->get_title( 0, $dots = '', $mod, $read_cache = true,
+					$add_hashtags = false, $do_encode = true, $md_idx = 'schema_title', $sep = false );
 
 				$item_url = $wpsso->util->get_canonical_url( $mod );
 
