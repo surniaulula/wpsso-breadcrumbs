c27ea066aaebd0ef3a19f4346c3e82516ee9a996
JS Morisset <jsm@surniaulula.com>
2018-12-15 17:41:08 -0600

    v1.6.0

--- lib/gettext-config.php
+++ lib/gettext-config.php
@@ -1,4 +1,7 @@
 <?php die( 'These aren\'t the droids you\'re looking for...' );
-// extract of translatable text strings from static config array
+
+/**
+ * Extract of translatable text strings from the static config array.
+ */
 _x( 'Breadcrumbs', 'lib file description', 'wpsso-breadcrumbs' );
 _x( 'WPSSO Core add-on to provide JSON-LD formatted Schema BreadcrumbList markup for Google and Search Engine Optimization (SEO).', 'plugin description', 'wpsso-breadcrumbs' );
