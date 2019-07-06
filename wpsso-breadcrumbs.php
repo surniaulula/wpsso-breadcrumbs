7e68ced9845e48713facb39ac348534f6e4e134f
JS Morisset <jsm@surniaulula.com>
2019-07-06 09:33:12 -0700

    v2.1.0

--- wpsso-breadcrumbs.php
+++ wpsso-breadcrumbs.php
@@ -13,7 +13,7 @@
  * Description: WPSSO Core add-on offers Schema BreadcrumbList markup using Google's preferred JSON-LD format for Rich Results SEO.
  * Requires At Least: 3.8
  * Tested Up To: 5.2.2
- * Version: 2.1.0-rc.2
+ * Version: 2.1.0
  *
  * Version Numbering: {major}.{minor}.{bugfix}[-{stage}.{level}]
  *
