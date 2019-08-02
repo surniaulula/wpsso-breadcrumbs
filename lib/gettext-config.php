4e539e851485baa0090a2934f18cf1dc2116da51
JS Morisset <jsm@surniaulula.com>
2019-08-02 14:22:49 -0700

    v2.1.0

--- lib/gettext-config.php
+++ lib/gettext-config.php
@@ -4,4 +4,4 @@
  * Extract of translatable text strings from the static config array.
  */
 _x( 'Breadcrumbs', 'lib file description', 'wpsso-breadcrumbs' );
-_x( 'WPSSO Core add-on offers Schema BreadcrumbList markup using Google\'s JSON-LD standard for better SEO Rich Results.', 'plugin description', 'wpsso-breadcrumbs' );
+_x( 'Schema BreadcrumbList markup in JSON-LD for better Google SEO Rich Results.', 'plugin description', 'wpsso-breadcrumbs' );
