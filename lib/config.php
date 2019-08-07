eb4a4feadc1740f17cb14e9aabade324061135df
JS Morisset <jsm@surniaulula.com>
2019-08-07 07:44:22 -0700

    v2.1.1

--- lib/config.php
+++ lib/config.php
@@ -16,7 +16,7 @@ if ( ! class_exists( 'WpssoBcConfig' ) ) {
 		public static $cf = array(
 			'plugin' => array(
 				'wpssobc' => array(			// Plugin acronym.
-					'version'     => '2.1.1-rc.1',	// Plugin version.
+					'version'     => '2.1.1',	// Plugin version.
 					'opt_version' => '3',		// Increment when changing default option values.
 					'short'       => 'WPSSO BC',	// Short plugin name.
 					'name'        => 'WPSSO Schema Breadcrumbs Markup',
@@ -29,7 +29,7 @@ if ( ! class_exists( 'WpssoBcConfig' ) ) {
 					'req' => array(
 						'short'       => 'WPSSO Core',
 						'name'        => 'WPSSO Core',
-						'min_version' => '5.5.1-rc.1',
+						'min_version' => '5.5.1',
 					),
 					'assets' => array(
 						'icons' => array(
