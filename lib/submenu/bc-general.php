08782700bf1c7088a37fba427ca81cb2599af88f
JS Morisset <jsm@surniaulula.com>
2020-04-14 17:01:47 -0700

    v2.4.0

--- lib/submenu/bc-general.php
+++ lib/submenu/bc-general.php
@@ -88,7 +88,8 @@ if ( ! class_exists( 'WpssoBcSubmenuBcGeneral' ) && class_exists( 'WpssoAdmin' )
 					$bc_select_for_posts = '';
 					$bc_select_for_terms = '';
 
-					foreach ( $this->p->util->get_post_types( 'objects' ) as $pt ) {
+					foreach ( $this->p->util->get_post_types( 'objects' ) as $pt ) {	// Get public post types.
+
 						$bc_select_for_posts .= '<p>' . $this->form->get_select( 'bc_list_for_ptn_' . $pt->name, $bc_list_for_posts ) . ' ' .
 							$pt->label . ( empty( $pt->description ) ? '' : ' (' . $pt->description . ')' ) . '</p>';
 					}
