fd51696f1604b46c1df8f25f1465e9a91466d69d
JS Morisset <jsm@surniaulula.com>
2020-04-27 13:21:21 -0700

    v2.5.0

--- lib/config.php
+++ lib/config.php
@@ -52,10 +52,6 @@ if ( ! class_exists( 'WpssoBcConfig' ) ) {
 					 * Library files loaded and instantiated by WPSSO.
 					 */
 					'lib' => array(
-						'pro' => array(
-						),
-						'std' => array(
-						),
 						'submenu' => array(
 							'bc-general' => 'Breadcrumbs',
 						),
