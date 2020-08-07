3a8d60666302aa6592a605085cb82dd0b9ef4be3
JS Morisset <jsm@surniaulula.com>
2020-08-07 09:53:35 -0700

    v2.9.0-dev.7

--- lib/submenu/bc-general.php
+++ lib/submenu/bc-general.php
@@ -66,7 +66,7 @@ if ( ! class_exists( 'WpssoBcSubmenuBcGeneral' ) && class_exists( 'WpssoAdmin' )
 				$table_rows = apply_filters( $filter_name, $this->get_table_rows( $metabox_id, $tab_key ), $this->form );
 			}
 
-			$this->p->util->do_metabox_table( $table_rows, 'metabox-' . $metabox_id . '-' . $tab_key );
+			$this->p->util->metabox->do_table( $table_rows, 'metabox-' . $metabox_id . '-' . $tab_key );
 		}
 
 		protected function get_table_rows( $metabox_id, $tab_key ) {
