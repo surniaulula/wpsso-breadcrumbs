d9737e80bec62889879002bdb4e04670206b9fd2
JS Morisset <jsm@surniaulula.com>
2020-09-13 10:50:09 -0700

    v3.0.0

--- lib/gettext-config.php
+++
@@ -1,7 +0,0 @@
-<?php die( 'These aren\'t the droids you\'re looking for.' );
-
-/**
- * Extract of translatable text strings from the static config array.
- */
-_x( 'Breadcrumbs', 'lib file description', 'wpsso-breadcrumbs' );
-_x( 'Schema BreadcrumbList Markup in JSON-LD for Better Google Rich Results with Structured Data.', 'plugin description', 'wpsso-breadcrumbs' );
