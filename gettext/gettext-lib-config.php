435245523105970594307b4fd230737ee6554621
JS Morisset <jsm@surniaulula.com>
2020-09-15 09:14:50 -0700

    v3.0.0

--- gettext/gettext-lib-config.php
+++ gettext/gettext-lib-config.php
@@ -4,4 +4,5 @@
  * Extract of translatable strings from lib/config.php.
  */
 _x( 'Breadcrumbs', 'lib file description', 'wpsso-breadcrumbs' );
+_x( 'WPSSO Schema Breadcrumbs Markup', 'plugin name', 'wpsso-breadcrumbs' );
 _x( 'Schema BreadcrumbList Markup in JSON-LD for Better Google Rich Results with Structured Data.', 'plugin description', 'wpsso-breadcrumbs' );
