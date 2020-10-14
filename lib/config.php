107cc33f31fc4a8495c9bb2def883756b9cbb6d5
JS Morisset <jsm@surniaulula.com>
2020-10-14 13:47:26 -0700

    v3.1.0-dev.6

--- lib/config.php
+++ lib/config.php
@@ -17,7 +17,7 @@ if ( ! class_exists( 'WpssoBcConfig' ) ) {
 		public static $cf = array(
 			'plugin' => array(
 				'wpssobc' => array(			// Plugin acronym.
-					'version'     => '3.1.0-dev.5',	// Plugin version.
+					'version'     => '3.1.0-dev.6',	// Plugin version.
 					'opt_version' => '7',		// Increment when changing default option values.
 					'short'       => 'WPSSO BC',	// Short plugin name.
 					'name'        => 'WPSSO Schema Breadcrumbs Markup',
@@ -37,7 +37,7 @@ if ( ! class_exists( 'WpssoBcConfig' ) ) {
 							'home'          => 'https://wordpress.org/plugins/wpsso/',
 							'plugin_class'  => 'Wpsso',
 							'version_const' => 'WPSSO_VERSION',
-							'min_version'   => '8.8.0-dev.5',
+							'min_version'   => '8.8.0-dev.6',
 						),
 					),
 
