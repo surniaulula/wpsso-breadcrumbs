f9382593dc9de23541be445eb046dd9e61af8685
JS Morisset <jsm@surniaulula.com>
2020-10-18 11:08:09 -0700

    v3.1.1

--- wpsso-breadcrumbs.php
+++ wpsso-breadcrumbs.php
@@ -68,7 +68,10 @@ if ( ! class_exists( 'WpssoBc' ) ) {
 			load_plugin_textdomain( 'wpsso-breadcrumbs', false, 'wpsso-breadcrumbs/languages/' );
 		}
 
-		public function init_objects( $is_admin ) {
+		/**
+		 * $is_admin, $doing_ajax, and $doing_cron available since WPSSO Core v8.8.0.
+		 */
+		public function init_objects( $is_admin = false, $doing_ajax = false, $doing_cron = false ) {
 
 			$this->p =& Wpsso::get_instance();
 
