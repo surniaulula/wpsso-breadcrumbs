f79acce9f6d7a79cb576f839c9b35465ffc2a629
JS Morisset <jsm@surniaulula.com>
2021-07-01 14:25:26 -0700

    v3.5.0

--- wpsso-breadcrumbs.php
+++ wpsso-breadcrumbs.php
@@ -12,7 +12,7 @@
  * License URI: https://www.gnu.org/licenses/gpl.txt
  * Description: Schema BreadcrumbList markup in JSON-LD format for Google Rich Results.
  * Requires PHP: 7.0
- * Requires At Least: 4.5
+ * Requires At Least: 5.0
  * Tested Up To: 5.7.2
  * Version: 3.5.0
  *
