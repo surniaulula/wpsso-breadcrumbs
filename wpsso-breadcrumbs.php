a3f0d8c86851c0f8b3595d0395185108708fbe56
JS Morisset <jsm@surniaulula.com>
2021-09-09 05:15:57 -0700

    v3.5.0

--- wpsso-breadcrumbs.php
+++ wpsso-breadcrumbs.php
@@ -13,7 +13,7 @@
  * Description: Schema BreadcrumbList markup in JSON-LD format for Google Rich Results.
  * Requires PHP: 7.0
  * Requires At Least: 5.0
- * Tested Up To: 5.8
+ * Tested Up To: 5.8.1
  * Version: 3.5.0
  *
  * Version Numbering: {major}.{minor}.{bugfix}[-{stage}.{level}]
