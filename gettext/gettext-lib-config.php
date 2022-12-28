c6c113001b7cb1de13404079e852b54d0fb575ad
JS Morisset <jsm@surniaulula.com>
2022-12-28 12:02:23 -0800

    v4.2.0

--- gettext/gettext-lib-config.php
+++ gettext/gettext-lib-config.php
@@ -1,8 +1,7 @@
 <?php die( 'These aren\'t the droids you\'re looking for.' );
-
-/**
- * Extract of translatable strings from lib/config.php.
- */
+// 
+// Extract of translatable strings from lib/config.php.
+// 
 _x( 'Breadcrumbs', 'lib file description', 'wpsso-breadcrumbs' );
 _x( 'WPSSO Schema Breadcrumbs Markup', 'plugin name', 'wpsso-breadcrumbs' );
 _x( 'Schema BreadcrumbList markup in JSON-LD format for Google Rich Results.', 'plugin description', 'wpsso-breadcrumbs' );
