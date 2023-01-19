b7e79f87f20a52a31c2f461944897406653d64a2
JS Morisset <jsm@surniaulula.com>
2023-01-19 09:45:57 -0800

    v4.3.0-rc.2

--- wpsso-breadcrumbs.php
+++ wpsso-breadcrumbs.php
@@ -1,5 +1,5 @@
 <?php
-/**
+/*
  * Plugin Name: WPSSO Schema Breadcrumbs Markup
  * Plugin Slug: wpsso-breadcrumbs
  * Text Domain: wpsso-breadcrumbs
@@ -15,7 +15,7 @@
  * Requires PHP: 7.2
  * Requires At Least: 5.2
  * Tested Up To: 6.1.1
- * Version: 4.3.0-rc.1
+ * Version: 4.3.0-rc.2
  *
  * Version Numbering: {major}.{minor}.{bugfix}[-{stage}.{level}]
  *
