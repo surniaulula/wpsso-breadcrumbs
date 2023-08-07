12b43a887b0bf765c5caab3ecbec28dcc6a2c345
JS Morisset <jsm@surniaulula.com>
2023-08-07 11:31:59 -0700

    v4.3.2

--- lib/register.php
+++ lib/register.php
@@ -107,7 +107,7 @@ if ( ! class_exists( 'WpssoBcRegister' ) ) {
 				/*
 				 * Register plugin install, activation, update times.
 				 */
-				if ( class_exists( 'WpssoUtilReg' ) ) { // Since WPSSO Core v6.13.1.
+				if ( class_exists( 'WpssoUtilReg' ) ) {	// Since WPSSO Core v6.13.1.
 
 					$version = WpssoBcConfig::get_version();
 
