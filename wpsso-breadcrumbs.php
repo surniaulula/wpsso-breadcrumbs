9bd16d544e1593561d32e63158584814398a82f5
jsmoriss <jsm@surniaulula.com>
2025-12-23 17:41:21 +0000

    v5.5.0-rc.1

--- wpsso-breadcrumbs.php
+++ wpsso-breadcrumbs.php
@@ -15,7 +15,7 @@
  * Requires PHP: 7.4.33
  * Requires At Least: 5.9
  * Tested Up To: 6.9
- * Version: 5.5.0-b.3
+ * Version: 5.5.0-rc.1
  *
  * Version Numbering: {major}.{minor}.{bugfix}[-{stage}.{level}]
  *
